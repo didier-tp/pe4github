440f97b270c4deb5118db677a93df7cd9831904d
Gaudin Farid <MarchandDeSable@hotmail.fr>
2018-05-15 11:23:35 +0200

    Message : changement de branche

--- projetFarid-Ornella/src/main/java/pe4/AppCommun.java
+++ projetFarid-Ornella/src/main/java/pe4/AppCommun.java
@@ -4,7 +4,7 @@ public class AppCommun {
 
 	public static void main(String[] args) {
 		System.out.println("application commun de Farid et Ornella!!");
-
+		System.out.println("application commun de Farid avec la branche ornella");
 	}
 
 }
