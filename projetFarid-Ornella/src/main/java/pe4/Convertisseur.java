a542ded2e571ae6e241cb5f46e3e9265ee7f9f05
Damiani Ornella <nell.damiani@gmail.com>
2018-05-16 10:31:48 +0200

    erreur corrigée

--- projetFarid-Ornella/src/main/java/pe4/Convertisseur.java
+++ projetFarid-Ornella/src/main/java/pe4/Convertisseur.java
@@ -3,7 +3,7 @@ package pe4;
 public class Convertisseur {
 	
 	public double euroToFranc(double montantEnEuro) {
-		return montantEnEuro*16.55957;
+		return montantEnEuro*6.55957;
 	}
 	
 	public double FrancToEuro(double montantEnFranc) {
